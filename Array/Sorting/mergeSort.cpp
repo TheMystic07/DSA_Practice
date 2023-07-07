cd6c6689ada6fc829ac45e69310de183e771e725
Mystic <gushabad90@gmail.com>
2023-07-08 00:51:52 +0530

    Learned Merge Sort (i guess?)

---
+++ Array/Sorting/mergeSort.cpp
@@ -0,0 +1,45 @@
+#include "SortingFuncs.cpp"
+#include <iostream>
+void merge(int * arr,int low,int mid ,int high){
+  int left=low;
+  int right=mid+1;
+  int temp[high];
+  int i=0;
+  while (left<=mid && right<=high){
+    if (arr[left]<=arr[right]){
+      temp[i]=arr[left];
+      left++;i++;
+    }else{
+      temp[i]=arr[right];
+      right++;i++;
+    }
+    }
+    while(left<=mid){
+      temp[i]=arr[left];
+      left++;i++;}
+    while(right<=high){
+      temp[i]=arr[right];
+      right++;i++;
+    }
+  for(int j=low;j<=high;j++){
+    arr[j]=temp[j-low];
+  }
+  
+}
+
+void mergeSort(int *arr,int low,int high){
+  if(low>=high)return;
+  int mid=(low+high)/2;
+  mergeSort(arr,low,mid);
+  mergeSort(arr,mid+1,high);
+  merge(arr,low,mid,high);
+}
+
+
+int main (int argc, char *argv[]) {
+  int * arr=getArray(5,0,10);
+  printArray(arr,5);
+  mergeSort(arr,0,4);
+  printArray(arr,5);
+  return 0;
+} 
